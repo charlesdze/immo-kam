14c9ce0c26c66bcfc4e1f8772add5ecab6196d4e
Dsimo Dze Charles Agnor <charlesdze64@gmail.com>
2026-05-16 22:52:53 +0100

    fix: adjust mysql properties for direct environment reading

--- config/database.php
+++ config/database.php
@@ -13,7 +13,6 @@ return [
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
     |
-    |
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
@@ -48,8 +47,8 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'railway'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -89,8 +88,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
@@ -104,7 +101,6 @@ return [
     | your application. Using this information, we can determine which of
     | the migrations on disk haven't actually been run in the database.
     |
-    |
     */
 
     'migrations' => 'migrations',
@@ -115,9 +111,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
+    | provides a richer body of commands than a typical key-value system.
     |
     */
 
